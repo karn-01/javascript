565fffc8d6376b857529db0ef578c5b00da82af4
karn-01 <mohitsaroya001@gmail.com>
2024-12-15 14:39:28 +0000

    conversionexamples

---
+++ 01_basics/practice.js
@@ -0,0 +1,40 @@
+
+console.log(3+3)// 6
+console.log(3/4) //having full value==0.75 not like the int 
+console.log(3/3)//1
+console.log(2%3) //2
+
+
+console.log("1"  +  3 ); //here the 1 is the string and then then number
+// string phele to conver all into the string //13
+console.log(1+"3");  //13
+console.log("1"  + 2 + 2); //122  //all into the string
+
+console.log(1 + 2 + "2")  //now phele number to + hoga then string ka
+//32
+
+// console.log( 3 + 4 *4 /43 );  // use the paranthesis
+// console.log(  ((3+4)*4) / 43  );
+
+console.log(true)  //true
+console.log(+true)  //1
+
+let num1,num2,num3
+num1=num2=num3=2+3
+console.table([num1,num2,num3])
+
+let gamecounter=100
+gamecounter++ //101
+console.log(gamecounter)
+
+let num5=gamecounter  //101
+let num6=++gamecounter //num6 = hogio 102
+let num7 = gamecounter++ // num7= hogi 102 then increses to 103 
+console.log(num5)
+console.log(num6)
+
+console.log(num7)
+
+// now the gamecounter is 103 
+let final=gamecounter //103
+console.log(final)
