808839c6f690f466c996b8df6c7bcee1015189f4
karn-01 <mohitsaroya001@gmail.com>
2024-12-15 13:42:45 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,24 @@
+console.log("Mohit")          //to print console.log()
+const name="mohit"            //constant value not change 
+let email="mohitsaroya2021@gmail.com"    // now use this let 
+var password="12345"   
+
+/*     we dont prefer the var vbeacuse of issue in block scope and functional scope
+       we use the let insted of var
+*/
+
+//name="yo"  pulls an error to change the const variable value
+
+
+console.log(name);  
+
+console.table([name,email,password])    // print in tabular form console.table([      to print there name   ])
+
+email="yo"
+console.log(email);
+console.table([name,email,password])
+
+cityname="delhi"   //we can sometime make the variable without giving it type
+console.log(cityname)
+
+let cityarea;  //out of this  using the console.log() is undefined 
--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,1 +1,29 @@
-"use strict"    //treat all js code as the newer version
+"use strict " //treat all js code as a newer version
+// alertt(3+3)  calling the nodejs not the browser
+
+
+console.log(3+3) //6
+// code readability should be high
+
+
+let name="mohit"  // string
+let age= 19   // number r
+let islogein=false  //boolean true ya false
+
+
+/* number  -- 3 to power 53
+bigint
+string
+boolean   true ya false
+null== standalone value like empty value khali
+undefined == value ja humne define nhi ki value declared only 
+symbol  use to find the uniqueness (unique)
+imp     object    
+*/
+console.log(typeof "mohit")  // string ha ya 
+console.log(typeof 5)  // number
+
+
+console.log(typeof null)  // object  
+
+console.log(typeof undefined)  // undefined type hi ye ha
