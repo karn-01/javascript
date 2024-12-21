2bfdc402ebc6882b48aa286bf405ace922a82f55
karn-01 <mohitsaroya001@gmail.com>
2024-12-21 20:18:56 +0000

    object3

---
+++ 02_basics/05_object3.js
@@ -0,0 +1,34 @@
+//de structuring     //[] array
+const course = {
+    courname: "js hindi",
+    price: "999",
+    courseinstructor: "mohit"
+
+    
+}
+// course.courseinstructor
+//destructure
+
+// const {courseinstructor} = course
+// console.log(courseinstructor);  //mohit
+
+
+const {courseinstructor:instructor} = course
+console.log(instructor); //mohit
+
+//api kisi aur pe kam dalna 
+//ex login pe click google khud dekhaga 
+
+//jason
+// {
+//     "name":"mohit",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+
+{
+    {}
+    {}
+}
+//randomuser me api
